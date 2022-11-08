52a3b906dfcda22a28a27184e0a216168ac5f520
Paulo-Piotto <piotto@pop-os.localdomain>
2022-11-08 11:07:04 -0300

    add new store, find all and search store for name implemented

---
+++ pilot-back/src/repositories/storesRepository.js
@@ -0,0 +1,25 @@
+import client from "../database.js";
+
+async function create(newStore) {
+   const result = await client.stores.create({
+     data: {
+      name: newStore.name
+     }
+   });
+}
+
+async function find(storeName) {
+   const result = await client.stores.findUnique({
+      where: {
+        name: storeName,
+      },
+    })
+   return result;
+}
+
+async function findAll() {
+   const result = await client.stores.findMany()
+   return result;
+}
+
+export { create, find, findAll };
---
+++ pilot-back/src/routes/storesRouter.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import * as storesController from "../controllers/storesController.js";
+import * as schemaValidationMiddleware from '../middlewares/schemaValidationMiddleware.js'
+import storeSchema from "../schemas/storeSchema.js";
+
+const storeRouter = Router();
+const PATH = "/stores";
+
+storeRouter.post(`${PATH}`,
+  (req, res, next) => schemaValidationMiddleware.test(req, res, next, storeSchema),
+  storesController.post
+);
+
+storeRouter.get(`${PATH}`,
+  storesController.findAll
+);
+
+storeRouter.get(
+  `${PATH}/:name`,
+  storesController.find
+);
+
+export default storeRouter;
